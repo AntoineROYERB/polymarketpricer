d4a6aa1ddd3c9b08a00fe7d78b7397ed35b352ae
Antoine <royerb.antoine@gmail.com>
2026-06-17 20:08:33 +0200

    chore: refresh seed dump

--- docker/initdb/seed.sql
+++ docker/initdb/seed.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5ebc5993cf9c595a66a965cc4b55b42d7cb9969f5da8bb04493d27a74396158
-size 117523257
+oid sha256:b258fbec7fd98dd449378bfa81bc6168e680a980e3e56ef15f1f5f8717fa0bea
+size 171230570
