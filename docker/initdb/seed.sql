dfe6e60b3a7969e1a9819f1958d40e107c2d0f04
Antoine <royerb.antoine@gmail.com>
2026-06-19 17:27:38 +0200

    chore: add category analytics backfill script and update seed
    - Backfill script retroactively classifies all existing markets by category and populates mapped_category
    - Refreshed seed dump includes category data for faster dev setup

--- docker/initdb/seed.sql
+++ docker/initdb/seed.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b258fbec7fd98dd449378bfa81bc6168e680a980e3e56ef15f1f5f8717fa0bea
-size 171230570
+oid sha256:563e3dfec1d55be893f5d47998cd5fbf95baa640dcbc5d405a924c50f839c7e9
+size 172900610
